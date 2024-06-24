7b65bb41c0fd618ac7661e579313ad1a40f7809c
Dhruv Shukla <135282874+DhruvShukla01@users.noreply.github.com>
2024-06-24 18:45:37 +0530

    added work in progress gif

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+document.addEventListener('DOMContentLoaded', () => {
+    const wipMessage = document.createElement('div');
+    wipMessage.id = 'wip-message';
+
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://giphy.com/embed/JomVilU47wifhL9FSQ';
+    iframe.width = '100%';
+    iframe.height = '100%';
+    iframe.style.position = 'absolute';
+    iframe.frameBorder = '0';
+    iframe.classList.add('giphy-embed');
+    iframe.allowFullscreen = true;
+
+    const gifContainer = document.createElement('div');
+    gifContainer.style.width = '50px'; // Adjust size as needed
+    gifContainer.style.height = '50px'; // Adjust size as needed
+    gifContainer.style.position = 'relative';
+    gifContainer.style.paddingBottom = '0';
+    gifContainer.style.marginRight = '10px';
+
+    gifContainer.appendChild(iframe);
+
+    const text = document.createElement('span');
+    text.innerText = 'Work in Progress';
+
+    wipMessage.appendChild(gifContainer);
+    wipMessage.appendChild(text);
+    document.body.appendChild(wipMessage);
+});
